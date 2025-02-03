4d0e058482132db5b5e31af05ec3c85f2ca83d8a
unknown <manishmt007@gmail.com>
2025-02-03 21:31:57 +0530

    updated code

--- src/Route/MyRoute.jsx
+++ src/Route/MyRoute.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-// import Dashboard from "./Pages/useraccount/Dashboard";
 import Login from "../Pages/useraccount/Login";
 import Dashboard from "../Pages/useraccount/Dashboard";
 
